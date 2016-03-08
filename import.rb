c58512eaef1e9ae8919bb43fe9c4386af42ec692
concreteface <concreteface@users.noreply.github.com>
2016-03-08 14:46:56 -0500

    about to set up table schema

--- import.rb
+++ import.rb
@@ -4,6 +4,7 @@
 require "pg"
 require 'csv'
 require 'pry'
+require_relative 'id_finder'
 
 def db_connection
   begin
@@ -12,10 +13,6 @@ def db_connection
     ensure
     connection.close
   end
-end
-
-CSV.foreach('sales.csv', headers:true) do |row|
-  db_connection {|conn| conn.exec_params("INSERT INTO employee (name_email) VALUES ($1)", [row['employee']] )}
 end
 
 product_contents = []
@@ -39,22 +36,30 @@ CSV.foreach('sales.csv', headers:true) do |row|
     product_contents << content['name_number']
   end
 end
-# CSV.foreach('sales.csv', headers:true) do |row|
-#   # binding.pry
-#   if !product_contents.include?(row['customer_and_account_no'])
-#     db_connection {|conn| conn.exec_params("INSERT INTO customer_acct_num (name_number) VALUES ($1)", [row['customer_and_account_no']] )}
-#   end
-#   result = db_connection {|conn| conn.exec("SELECT name_number FROM customer_acct_num;")}
-#   result.each do |content|
-#     binding.pry
-#     product_contents << content['name_number']
-#   end
-# end
 
+product_contents = []
 CSV.foreach('sales.csv', headers:true) do |row|
-  db_connection {|conn| conn.exec_params("INSERT INTO frequency (invoice_frequency) VALUES ($1)", [row['invoice_frequency']] )}
+  if !product_contents.include?(row['employee'])
+    db_connection {|conn| conn.exec_params("INSERT INTO employee (name_email) VALUES ($1)", [row['employee']] )}
+  end
+  result = db_connection {|conn| conn.exec("SELECT name_email FROM employee;")}
+  result.each do |content|
+    product_contents << content['name_email']
+  end
 end
 
-# CSV.foreach('sales.csv', headers:true) do |row|
-#   db_connection {|conn| conn.exec_params("INSERT INTO sales (sale_date, sale_amount, units_sold, invoice_no) VALUES ($1, $2, $3, $4)", [row['sale_date'], row['sale_amount'], row['units_sold'], row['invoice_no'] )}
-# end
+product_contents = []
+CSV.foreach('sales.csv', headers:true) do |row|
+  if !product_contents.include?(row['invoice_frequency'])
+    db_connection {|conn| conn.exec_params("INSERT INTO frequency (invoice_frequency) VALUES ($1)", [row['invoice_frequency']] )}
+  end
+  result = db_connection {|conn| conn.exec("SELECT invoice_frequency FROM frequency;")}
+  result.each do |content|
+    product_contents << content['invoice_frequency']
+  end
+end
+
+
+CSV.foreach('sales.csv', headers:true) do |row|
+  db_connection {|conn| conn.exec_params("INSERT INTO sales (sale_date, sale_amount, units_sold, invoice_no) VALUES ($1, $2, $3, $4, #{id_finder[row["employee"], 'employee', 'name_email']}, #{id_finder[row["customer_and_account_no"], 'customer_acct_num', 'name_number']}, #{id_finder[row["product_name"], 'product', 'product_name']}, #{id_finder[row["invoice_frequency"], 'frequency', 'invoice_frequency']})", [row['sale_date'], row['sale_amount'], row['units_sold'], row['invoice_no']] )}
+end
--- tester.rb
+++ tester.rb
@@ -10,18 +10,18 @@ def db_connection
     connection.close
   end
 end
-#
-# # sales: sale_date, sale_amount, units_sold, invoice_num
-#
-#
-# CSV.foreach('sales.csv', headers:true) do |row|
-#   db_connection {|conn| conn.exec_params("INSERT INTO sales (employee_name, customer_acct_number, product_name, sale_date, sale_amount, units_sold, invoice_num, invoice_freq) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)", [row['employee'],row['customer_and_account_no'], row['product_name'], row['sale_date'], row['sale_amount'], row['units_sold'], row['invoice_no'], row['invoice_frequency']] )}
+
+
+result = db_connection {|conn| conn.exec("SELECT * FROM product WHERE product_name = 'Chimp Glass'")}
+
+# result.each do |res|
+#   puts res['id']
 # end
+puts result[0]['id']
 
-result = db_connection {|conn| conn.exec("SELECT product_name FROM product;")}
-product_contents = []
-result.each do |content|
-product_contents << content
-end
 
-puts product_contents
+
+
+
+
+# "SELECT * FROM product WHERE product_name = 'Chimp Glass'"
